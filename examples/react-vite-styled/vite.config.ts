ed450c251615f6e41cf3637c350c36b904aca3ca
fsubal <fsubal1102@gmail.com>
2022-03-26 00:16:55 +0900

    add vite plugin

--- examples/react-vite-styled/vite.config.ts
+++ examples/react-vite-styled/vite.config.ts
@@ -1,17 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import dynamicImport from "@rollup/plugin-dynamic-import-vars";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ["@charcoal-ui/icons"],
-  },
-  build: {
-    rollupOptions: {
-      // for @charcoal-ui/icons
-      plugins: [dynamicImport()],
-    },
-  },
-  plugins: [react()],
+  plugins: [react(), charcoalIcons()],
 });
+
+/**
+ * vite で @charcoal-ui/icons が動くようにするプラグイン
+ */
+function charcoalIcons(): PluginOption {
+  return {
+    name: "charcoal-icons",
+    config() {
+      return {
+        optimizeDeps: {
+          // 開発環境の Pre-bundling で壊れる
+          // https://vitejs.dev/guide/dep-pre-bundling.html#the-why
+          exclude: ["@charcoal-ui/icons"],
+        },
+        build: {
+          rollupOptions: {
+            // dynamicImport がビルド時に解決されない
+            // `build.dynamicImportVarsOptions` を追加しても治らなかった
+            // https://vitejs.dev/config/#build-dynamicimportvarsoptions
+            plugins: [dynamicImport()],
+          },
+        },
+      };
+    },
+  };
+}
